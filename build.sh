4e5cbccb77476304b380ecc859610641df062202
BuildTools <unconfigured@null.spigotmc.org>
2019-05-06 23:54:08 +0200

    Bugfixes for gosu

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-IMAGE_NAME=${1:-"jshimko/meteor-launchpad"}
+IMAGE_NAME=${1:-"njbuch/meteor-launchpad"}
 
 printf "\n[-] Building $IMAGE_NAME...\n\n"
 
-docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
+## docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
 docker build -t $IMAGE_NAME:latest .
--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -25,32 +25,12 @@ apt-get install -y --no-install-recommends curl bzip2 bsdtar build-essential pyt
 
 
 # install gosu
-
-dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
-
-wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"
-wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch.asc"
-
-export GNUPGHOME="$(mktemp -d)"
-
-key="B42F6819007F00F88E364FD4036A9C25BF357DD4"
-
-# Try different key servers in case one is unresponsive
-# See: https://github.com/bodastage/bts-ce-database/issues/1
-for server in ha.pool.sks-keyservers.net \
-              hkp://p80.pool.sks-keyservers.net:80 \
-              keyserver.ubuntu.com \
-              hkp://keyserver.ubuntu.com:80 \
-              pgp.mit.edu; do
-    gpg --keyserver "$server" --recv-keys "${key}" && break || echo "Trying new server..."
-done
-
-gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
-
-rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
-
-chmod +x /usr/local/bin/gosu
-
+set -eux; \
+apt-get update; \
+apt-get install -y gosu; \
+rm -rf /var/lib/apt/lists/*; \
+# verify that the binary works
 gosu nobody true
+
 
 apt-get purge -y --auto-remove wget
